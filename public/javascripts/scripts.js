1bf267854c72496496763e79c5efc7aedcabcbc1
AbhijitHalder <66031400+AbhijitHalder@users.noreply.github.com>
2020-12-12 08:48:13 -0500

    Delete Confirmation

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,25 @@
+$('.delete').on('click', () =>
+{
+    return confirm('Are you sure you want to delete this?')
+})
+
+/*function comparePasswords()
+
+{
+    var pw1 = document.getElementById("password").value
+    var pw2 = document.getElementById("confirm").value
+    var pwMsg = document.getElementById("pwMsg")
+
+    if(pw1 != pw2)
+    {
+        pwMsg.innerHTML = "Passwords do NOT match"
+        pwMsg.className = "text-danger"
+        return false
+    }
+    else
+    {
+        pwMsg.innerHTML = ""
+        pwMsg.className = ""
+        return true
+    }
+}*/
